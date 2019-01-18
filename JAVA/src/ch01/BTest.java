2b95413997cd892d890869cba1ff60cde7329c8f
k <k@DESKTOP-3K15A12>
2019-01-18 17:51:50 +0900

    BTest 2nd commit 출력

--- JAVA/src/ch01/BTest.java
+++ JAVA/src/ch01/BTest.java
@@ -2,6 +2,7 @@ package ch01;
 
 public class BTest {
 	public static void main(String[] args) {
-		System.out.println("BTest 첫번째 커밋");
+		System.out.println("BTest 1st commit");
+		System.out.println("BTest 2nd commit");
 	}
 }
